4fe0dfc9cb4fac59ac2f096dd6628fac8a40ab66
dailey2 <98828394+dailey2@users.noreply.github.com>
2023-04-06 11:08:57 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -25,6 +25,7 @@ void addMessage(string username, string message, map<string,vector<string>> &mes
 	}
 }
 
+// Return messages for a certain user
 string getMessagesJSON(string username, map<string,vector<string>> &messageMap) {
 	/* retrieve json list of messages for this user */
 	bool first = true;
@@ -39,10 +40,36 @@ string getMessagesJSON(string username, map<string,vector<string>> &messageMap) 
 	return result;
 }
 
+// My function to get list of users
+void addUsers(string username, map<string,vector<string>> &messageMap, vector<string> userList) {
+	// Create a json list of current users
+	string jsonUser = "{\"user\":\""+username+"\"}";
+	for (auto userMessagePair : messageMap) {
+		username = userMessagePair.first;
+		userList[username].push_back(jsonUser);
+	}
+}
+
+// Return a json list of user 
+string getUsersJSON(string username, vector<string> &userList) {
+	/* retrieve json list of users */
+	bool first = true;
+	string result = "{\"user\":[";
+	for (string user :  userList[username]) {
+		if (not first) result += ",";
+		result += user;
+		first = false;
+	}
+	result += "]}";
+	userList[username].clear();
+	return result;
+}
+
 int main(void) {
   Server svr;
   int nextUser=0;
   map<string,vector<string>> messageMap;
+  vector<string> userList;
 	
   /* "/" just returnsAPI name */
   svr.Get("/", [](const Request & /*req*/, Response &res) {
@@ -54,41 +81,52 @@ int main(void) {
   svr.Get(R"(/chat/join/(.*))", [&](const Request& req, Response& res) {
     res.set_header("Access-Control-Allow-Origin","*");
     string username = req.matches[1];
-    string result;
+    string resultMessage;
+    string resultUser;
     vector<string> empty;
     cout << username << " joins" << endl;
     
     // Check if user with this name exists
     if (messageMap.count(username)) {
-    	result = "{\"status\":\"exists\"}";
+    	resultUser = "{\"status\":\"exists\"}";
     } else {
     	// Add user to messages map
     	messageMap[username]=empty;
-    	result = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
+    	resultMessage = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
+    	// Add user to user list
+    	userList[username]=empty;
+    	resultUser = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
     }
-    res.set_content(result, "text/json");
+    res.set_content(resultMessage, "text/json");
+    res.set_content(resultUser, "text/json");
   });
 
    svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
     res.set_header("Access-Control-Allow-Origin","*");
 	string username = req.matches[1];
 	string message = req.matches[2];
-	string result; 
+	string resultMessage; 
+	string resultUser; 
 	
     if (!messageMap.count(username)) {
-    	result = "{\"status\":\"baduser\"}";
+    	resultUser = "{\"status\":\"baduser\"}";
 	} else {
 		addMessage(username,message,messageMap);
-		result = "{\"status\":\"success\"}";
+		resultMessage = "{\"status\":\"success\"}";
+		addUsers(username,messageMap,userList);
+		resultUser = "{\"status\":\"success\"}";
 	}
-    res.set_content(result, "text/json");
+    res.set_content(resultMessage, "text/json");
+    res.set_content(resultUser, "text/json");
   });
   
    svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
     string username = req.matches[1];
     res.set_header("Access-Control-Allow-Origin","*");
-    string resultJSON = getMessagesJSON(username,messageMap);
-    res.set_content(resultJSON, "text/json");
+    string resultMessageJSON = getMessagesJSON(username,messageMap);
+    res.set_content(resultMessageJSON, "text/json");
+    string resultUsersJSON = getUsersJSON(username,userList);
+    res.set_content(resultUsersJSON, "text/json");
   });
   
   cout << "Server listening on port " << port << endl;
